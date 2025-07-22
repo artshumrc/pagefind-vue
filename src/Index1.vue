d696abbb9b913f110dde954de9f65315707de38d
David Flood <69060117+d-flood@users.noreply.github.com>
2025-07-22 17:18:23 -0400

    use/remove unused

--- src/Index1.vue
+++ src/Index1.vue
@@ -14,6 +14,26 @@
     :resultSort="resultSort"
     @update:searchQuery="onQueryChange"
   >
+    <template #collapse-title="{ direction, label }">
+      <h2 style="padding: 0; margin: 0">
+        {{ label }}
+        <span class="collapse-icon">
+          <svg
+            class="chevron-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            width="22"
+            height="22"
+            fill="currentColor"
+            viewBox="0 0 256 256"
+            :direction="direction"
+          >
+            <path
+              d="M213.66,101.66l-80,80a8,8,0,0,1-11.32,0l-80-80A8,8,0,0,1,53.66,90.34L128,164.69l74.34-74.35a8,8,0,0,1,11.32,11.32Z"
+            ></path>
+          </svg>
+        </span>
+      </h2>
+    </template>
   </Search>
 </template>
 
@@ -24,7 +44,6 @@ import type {
   FilterSortFunction as FilterOptionsSortFunction,
   CustomSortFunctions,
   FilterGroup,
-  FiltersDefinition,
   Filter,
   SortOption,
 } from './components/types'
