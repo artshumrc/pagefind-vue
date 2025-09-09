bd22861773fb26d50c6584876b8866e46d9d87f4
Ryan Muther <ryan_muther@fas.harvard.edu>
2025-09-09 15:43:22 -0400

    Added pagination at top of results section

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -8,6 +8,13 @@
       {{ activeFiltersText }}
     </div>
     <h2>{{ totalResultsCount }} Result<span v-if="totalResultsCount !== 1">s</span></h2>
+    <Pagination
+      :current-page="componentCurrentPage"
+      :total-items="props.results.length"
+      :items-per-page="props.itemsPerPage"
+      :reset-scroll-on-page-change="props.resetScrollOnPageChange"
+      @page-change="handlePageChange"
+    />
     <hr class="results-headder-content-separator" />
     <ul id="results-list">
       <li v-for="result in componentPageResults" :key="result?.raw_url">
